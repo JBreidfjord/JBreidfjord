7630079d6aae53babe9abccf100650e2329a717e
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-09-19 15:54:02 -0600

    fix border color

--- update_readme.py
+++ update_readme.py
@@ -1,9 +1,9 @@
 import json
 
-base_pin = '<a href="https://github.com/{username}/{repo}"><img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username={username}&repo={repo}&theme={theme}&hide_border=1" /></a>'
-base_stats = "<img align='center' src='https://github-readme-stats.vercel.app/api?username={username}&theme={theme}&include_all_commits=1&count_private=1&hide_border=1&hide_rank=1' />"
-base_lang_stats = "<img align='center' height='152' src='https://github-readme-stats.vercel.app/api/top-langs/?username={username}&langs_count={count}&theme={theme}&layout=compact&hide_border=1' />"
-base_wakatime = "<a href='https://wakatime.com/@{username}'><img align='center' height='152' src='https://github-readme-stats.vercel.app/api/wakatime?username={username}&theme={theme}&langs_count={count}&layout=compact&hide_border=1' /></a>"
+base_pin = '<a href="https://github.com/{username}/{repo}"><img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username={username}&repo={repo}&theme={theme}&border_color=#30363d" /></a>'
+base_stats = "<img align='center' src='https://github-readme-stats.vercel.app/api?username={username}&theme={theme}&include_all_commits=1&count_private=1&border_color=#30363d&hide_rank=1' />"
+base_lang_stats = "<img align='center' height='152' src='https://github-readme-stats.vercel.app/api/top-langs/?username={username}&langs_count={count}&theme={theme}&layout=compact&border_color=#30363d' />"
+base_wakatime = "<a href='https://wakatime.com/@{username}'><img align='center' height='152' src='https://github-readme-stats.vercel.app/api/wakatime?username={username}&theme={theme}&langs_count={count}&layout=compact&border_color=#30363d' /></a>"
 
 
 def replace_chunk(readme: str, content: str, marker: str) -> str:
